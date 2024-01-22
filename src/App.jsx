980391b4c7b27ded8412ff6801fa50690ebc161b
emeritolopes <emerito.lopes@gmail.com>
2024-01-22 22:30:19 +0000

    added routing in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react'
-import Home from './components/Home/Home'
-import Header from './components/Header/Header'
-import Contact from './components/Contact/Contact'
-import Project from './components/Projects/Project/Project'
+import Navbar from './components/Navbar/navbar';
+import Home from './components/Home/Home';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <>
-    <div> className="container-fluid d-flex flex-row justify-content-start"
-      <Router>
-        <Home />
-        <Routes>
-          <Route path="/" element={<div className='col-lg-10 col-md-8'><Home /></div>} />
-          <Route path="header" element={<div className='col-lg-10 col-md-8'><Header /></div>} /> 
-          <Route path="contacts" element={<div className='col-lg-10 col-md-8'><Contact /></div>} />
-          <Route path="project" element={<div className='col-lg-10 col-md-8'><Project /></div>} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="projects" element={<Project />} />
+        <Route path="contact/*" element={<Contact />} />
+      </Routes>
+    </Router>
+    <Footer />
     </>
   )
 }
 
-
-
-
-
-
-
-
-export default App
+export default App;
