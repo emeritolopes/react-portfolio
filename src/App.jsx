f25a727dc8fa4cf6e7a233208f5515895ac2a005
emeritolopes <emerito.lopes@gmail.com>
2024-01-22 07:56:32 +0000

    basic files created for this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
+import Contact from './components/Contact/Contact'
+import Project from './components/Projects/Project/Project'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div> className="container-fluid d-flex flex-row justify-content-start"
+      <Router>
+        <Home />
+        <Routes>
+          <Route path="/" element={<div className='col-lg-10 col-md-8'><Home /></div>} />
+          <Route path="header" element={<div className='col-lg-10 col-md-8'><Header /></div>} /> 
+          <Route path="contacts" element={<div className='col-lg-10 col-md-8'><Contact /></div>} />
+          <Route path="project" element={<div className='col-lg-10 col-md-8'><Project /></div>} />
+        </Routes>
+      </Router>
+    </div>
     </>
   )
 }
+
+
+
+
+
+
+
 
 export default App
